0cb1719aed109a36eb493fd63ad8d37dfe5923d2
vamshi137 <srinivasvamshi28@gmail.com>
2026-09-21 22:14:29 +0530

    Update FrequencyCountDigit implementation

--- Mixed_Logical_Challenges/Category_5_Applied_Reasoning_&_Real_Life_Logic/FrequencyCountDigit.java
+++ Mixed_Logical_Challenges/Category_5_Applied_Reasoning_&_Real_Life_Logic/FrequencyCountDigit.java
@@ -1,20 +1,43 @@
-class FrequencyCount{
-    public void Count(String num){
-        for(int i = 0;i<num.length();i++){
+class FrequencyCount {
+
+    public void Count(String num) {
+
+        for (int i = 0; i < num.length(); i++) {
+
+            boolean alreadyPrinted = false;
+
+            for (int k = 0; k < i; k++) {
+                if (num.charAt(i) == num.charAt(k)) {
+                    alreadyPrinted = true;
+                    break;
+                }
+            }
+
+            if (alreadyPrinted) {
+                continue;
+            }
+
             int count = 0;
-            for(int j =0;j<num.length();j++){
-                if(num.charAt(i) == num.charAt(j)){
+
+            for (int j = 0; j < num.length(); j++) {
+                if (num.charAt(i) == num.charAt(j)) {
                     count++;
                 }
             }
-            System.out.println(num.charAt(i)+":"+count);
+
+            System.out.println(num.charAt(i) + ":" + count);
         }
     }
 }
+
 public class FrequencyCountDigit {
+
     public static void main(String[] args) {
+
         FrequencyCount fc = new FrequencyCount();
+
         String num = "1223341";
+
         fc.Count(num);
     }
 }
