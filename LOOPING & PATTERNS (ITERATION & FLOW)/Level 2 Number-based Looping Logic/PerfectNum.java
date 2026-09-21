341c7705a4420a5a619224463c723c70a2b90175
vamshi137 <srinivasvamshi28@gmail.com>
2026-09-21 22:14:25 +0530

    Update PerfectNum implementation

--- LOOPING & PATTERNS (ITERATION & FLOW)/Level 2 Number-based Looping Logic/PerfectNum.java
+++ LOOPING & PATTERNS (ITERATION & FLOW)/Level 2 Number-based Looping Logic/PerfectNum.java
@@ -23,6 +23,7 @@ public class PerfectNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         CheckPerfect cp = new CheckPerfect();
+        System.out.println("enter number");
         System.out.println("enter the number to check whether it is perfect number or not");
         int num = sc.nextInt();
         System.out.println(cp.PrintPerfectNum(num));
